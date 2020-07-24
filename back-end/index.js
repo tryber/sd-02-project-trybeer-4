4674e1dc523a77473c8ef0246d92099366008fdd
miguel-miranda <ayandram7727@gmail.com>
2020-07-24 04:39:40 -0300

    uma pequena alteraçao no index

--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
+
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.listen(3001, () => console.log('Listening on 3001'));
