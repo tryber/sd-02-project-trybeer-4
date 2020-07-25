106689d5157eb85ddf89006d168054e4e4e50bde
miguel-miranda <ayandram7727@gmail.com>
2020-07-25 05:45:27 -0300

    CC

--- back-end/middlewares/otherErrorsHandler.js
+++ back-end/middlewares/otherErrorsHandler.js
@@ -5,7 +5,7 @@ const otherErrorsHandler = (err, _req, res, _next) => {
       message: err.message || 'Erro interno',
       code: err.code || 'internal_error',
     },
-  })
+  });
 };
 
 module.exports = otherErrorsHandler;
