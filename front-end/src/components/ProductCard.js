3b70a24bb5a9fcaa33796195c4e2d8f5d8a48e3e
miguel-miranda <ayandram7727@gmail.com>
2020-07-27 13:16:08 -0300

    CSS dos cards ja esta conforme o prototipo

--- front-end/src/components/ProductCard.js
+++ front-end/src/components/ProductCard.js
@@ -5,16 +5,24 @@ import '../styles/ProductCard.css';
 
 const ProductCard = ({ name, unitPrice, imageUrl, index }) => (
   <div className="product-card">
-    <p data-testid={`${index}-product-price`}>
+    <p
+      data-testid={`${index}-product-price`}
+      className="unit-price"
+    >
       {formatPrice(unitPrice)}
     </p>
-    <img
-      data-testid={`${index}-product-img`}
-      src={imageUrl}
-      alt={name}
+    <div className="product-image">
+      <img
+        data-testid={`${index}-product-img`}
+        src={imageUrl}
+        alt={name}
+      >
+      </img>
+    </div>
+    <p
+      data-testid={`${index}-product-name`}
+      className="product-name"
     >
-    </img>
-    <p data-testid={`${index}-product-name`}>
       {name}
     </p>
     <QuantityHandler index={index} />
