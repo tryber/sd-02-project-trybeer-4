0f3db3e0e7cac61f090ad73636a689a8e9cc16ee
miguel-miranda <ayandram7727@gmail.com>
2020-07-27 12:10:22 -0300

    commitando alteraçoes

--- front-end/src/components/ProductCard.js
+++ front-end/src/components/ProductCard.js
@@ -5,22 +5,18 @@ import '../styles/ProductCard.css';
 
 const ProductCard = ({ name, unitPrice, imageUrl, index }) => (
   <div className="product-card">
-    <h2
-      data-testid={`${index}-product-price`}
-    >
+    <p data-testid={`${index}-product-price`}>
       {formatPrice(unitPrice)}
-    </h2>
+    </p>
     <img
       data-testid={`${index}-product-img`}
       src={imageUrl}
       alt={name}
     >
     </img>
-    <h2
-      data-testid={`${index}-product-name`}
-    >
+    <p data-testid={`${index}-product-name`}>
       {name}
-    </h2>
+    </p>
     <QuantityHandler index={index} />
   </div>
 );
