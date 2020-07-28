73e0a1231c1800a7fae5fb62748d5ae620c7d3f2
guicgs <etohlif@protonmail.com>
2020-07-28 19:07:56 -0300

    CC

--- front-end/src/components/RegisterButton.js
+++ front-end/src/components/RegisterButton.js
@@ -9,23 +9,16 @@ function RegisterButton() {
     && nameValidation(name);
   return (
     <section className="register-btn-section">
-      {
-        validRegister === false
+      {validRegister === false
         && (
           <div>
             Digite um email válido e uma senha númerica de pelo menos 6 caracteres.
             <br />
             Seu nome deve conter, ao menos, 12 letras.
           </div>
-        )
-      }
+        )}
       {errorMessage && <div>{errorMessage}</div>}
-      <button
-        type="submit"
-        className="signup-btn"
-        data-testid="signup-btn"
-        disabled={!validRegister}
-      >
+      <button type="submit" className="signup-btn" data-testid="signup-btn" disabled={!validRegister}>
         CADASTRAR
       </button>
     </section>
