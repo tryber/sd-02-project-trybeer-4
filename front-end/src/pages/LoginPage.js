095aef8b005c4b80fe430812cfdf7454ada9d9fc
miguel-miranda <ayandram7727@gmail.com>
2020-07-29 13:20:46 -0300

    refatorando a pagina de login para diminuir complexidade e numero de linhas

--- front-end/src/pages/LoginPage.js
+++ front-end/src/pages/LoginPage.js
@@ -6,40 +6,43 @@ import LoginButton from '../components/LoginButton';
 import requestAPI from '../services/backEndAPI';
 import '../styles/LoginPage.css';
 
+function clearInputs(setEmail, setPassword) {
+  setEmail('');
+  setPassword('');
+}
+
 function getURL(role) {
   if (role === 'admin') return '/admin/orders';
   return '/products';
 }
 
-function clearInputs(setEmail, setPassword) {
-  setEmail('');
-  setPassword('');
+async function submitForm(event, body, setRedirect, setErrorMessage) {
+  event.preventDefault();
+  try {
+    const { data } = await requestAPI('POST', '/users/login', body);
+    localStorage.setItem('user', JSON.stringify(data));
+    const path = getURL(data.role);
+    setRedirect({ shouldRedirect: true, to: path });
+  } catch (error) {    
+    if (!error.response) return setErrorMessage('Erro de conexão com a API');
+    return setErrorMessage(error.response.data.error.message);
+  }
 }
 
 function LoginPage() {
   const { email, setEmail, password, setPassword, setErrorMessage } = useContext(LoginContext);
   const [redirect, setRedirect] = useState({ shouldRedirect: false, to: '' });
 
-  async function handleSubmit(e) {
-    e.preventDefault();
-    try {
-      const { data } = await requestAPI('POST', '/users/login', { email, password });
-      localStorage.setItem('user', JSON.stringify(data));
-      const path = getURL(data.role);
-      setRedirect({ shouldRedirect: true, to: path });
-    } catch (error) {    
-      if (!error.response) return setErrorMessage('Erro de conexão com a API');
-      return setErrorMessage(error.response.data.error.message);
-    }
-  }
+  const body = { email, password };
+  const handleSubmit = (event) => submitForm(event, body, setRedirect, setErrorMessage);
 
-  useEffect(() => (
-    () => clearInputs(setEmail, setPassword)
-  ), [setEmail, setPassword]);
+  useEffect(
+    () =>
+      () => clearInputs(setEmail, setPassword),
+    [setEmail, setPassword],
+  );
 
-  if (redirect.shouldRedirect) {
-    return <Redirect to={redirect.to} />
-  }
+  if (redirect.shouldRedirect) return <Redirect to={redirect.to} />;
 
   return (
     <form onSubmit={handleSubmit} className="login-form">
